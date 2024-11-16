ff09b2dc766978625e91debb9c9c7d0f179996aa
OgayoTK1 <a.ogayo@alustudent.com>
2024-11-16 21:51:11 +0300

    queries

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,8 @@
--- 15-comedy_only.sql
-SELECT tv_shows.title FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC;e
+-- RECORDS: List of all shows with their genre names
+SELECT  t.`title`, g.`name`
+ FROM `tv_shows` AS t
+	LEFT JOIN `tv_show_genres` AS s
+	ON t.`id` = s.`show_id`
+	LEFT JOIN `tv_genres` AS g
+	ON s.`genre_id` = g.`id`
+ORDER BY t.`title`, g.`name`;
