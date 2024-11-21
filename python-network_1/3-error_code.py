ed09770a8b74321aad098d569de60fe0da8e6268
OgayoTK1 <a.ogayo@alustudent.com>
2024-11-21 14:26:30 +0300

    developer

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""
-This script takes in a URL, sends a request to the URL and displays the body
-of the response (decoded in utf-8).
+"""scipt that intakes the url"""
 
-You have to manage urllib.error.HTTPError exceptions and print: Error code:
-followed by the HTTP status code.
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError, URLError
 
-Usage:
-    ./3-error_code.py <URL>
-"""
-
-import urllib.request
-import urllib.error
-import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    "lets begin"
+    url = argv[1]
+    req = Request(url)
+
     try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read()
+        with urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
+    except URLError as e:
+        print(e.reason)
