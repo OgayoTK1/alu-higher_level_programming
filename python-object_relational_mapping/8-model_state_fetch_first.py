adaedfd105a8bae09116512ad65474ff4d5b2b8a
OgayoTK1 <a.ogayo@alustudent.com>
2024-11-29 12:54:31 +0300

    model city fetch

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-""" prints the first state obj"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+"""
+Module to get all states
+"""
+from sys import argv
 
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    """connection"""
+    username, password, database = argv[1:4]
+    # default host is 'localhost' and default port is '3306'
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                sys.argv[1],
-                sys.argv[2],
-                sys.argv[3]
-                )
-            )
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(username, password, database),
+        pool_pre_ping=True
+    )
+    session = Session(engine)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    resul = session.query(State).first()
-    if resul is not None:
-        print(str(resul.id) + ":", resul.name)
+
+    state = session.query(State).first()
+    if state:
+        print("{}: {}".format(state.__dict__['id'], state.__dict__['name']))
     else:
         print("Nothing")
+
+    session.close()
