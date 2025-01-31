61d197b7631d647611574963e7c4adaecc32efc2
OgayoTK1 <a.ogayo@alustudent.com>
2025-01-31 16:12:13 +0200

    scopes closures

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,24 @@
 #!/usr/bin/node
-class Rectangle {
+// Script class Rectangle that defines a rectangle
+
+const Rectangle = class Rectangle {
   constructor (w, h) {
-    if (!w || !h || w <= 0 || h <= 0) return;
-    this.width = w;
-    this.height = h;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+    let prints = '';
+    for (let cont1 = 0; cont1 < this.height; cont1++) {
+      for (let cont = 0; cont < this.width; cont++) {
+        prints = prints + 'X';
+      }
+      console.log(prints);
+      prints = '';
     }
   }
-}
-module.exports = Rectangle;i
+};
+
+module.exports = Rectangle;
